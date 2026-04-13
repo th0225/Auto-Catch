cd0b4b99e80a3fa523d4f8c64e111a0418914452
TerryHung <excalibur0225@gmail.com>
2026-04-13 10:24:54 +0800

    feature: 建立頁面

---
+++ Models/SocialPost.cs
@@ -0,0 +1,10 @@
+public class SocialPost
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Platform { get; set; } = "Threads";
+    public string Author { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CapturedAt { get; set; } = DateTime.Now;
+    public string Url { get; set; } = string.Empty;
+    public List<string> Tags { get; set; } = [];
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AutoCatch;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// MudBlazor
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
